2e175a37ff356a87049689f1e2ca863bc3750080
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-28 00:55:19 +0600

    created: Forum component created

---
+++ src/components/Forum/Forum.js
@@ -0,0 +1,31 @@
+import classes from './Forum.module.css'
+
+const Forum = () => {
+    return (
+        <div className={classes.Forum}>
+            <h2>Desection Forum</h2>
+            <div>
+                <h4>Hello world</h4>
+                <p>- 12/12/2012</p>
+                <p>Rahat Hassan</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nobis!</p>
+            </div>
+
+            <div>
+                <h4>Patient Thoughts</h4>
+                <p>- 12/12/2012</p>
+                <p>Rahat Hassan</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nobis!</p>
+            </div>
+
+            <div>
+                <h4>New operation</h4>
+                <p>- 12/12/2012</p>
+                <p>Rahat Hassan</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nobis!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Forum
