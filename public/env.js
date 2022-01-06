4a70b25c8ec0576f25456958df8ff2c758b075f9
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-06 15:26:21 +0600

    env: env update from public

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 window.env = {
-    API: 'https://epapidev.healthx.com.bd',
-    EP_SITE: 'http://localhost:3002',
-}
+  "API": "https://epapidev.healthx.com.bd",
+  "EP_SITE": "http://localhost:3002"
+};
