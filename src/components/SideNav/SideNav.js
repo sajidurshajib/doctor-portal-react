8dff7ad2d0a631352648d12c60dce0b297bf8ff1
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-09 14:24:42 +0600

    redirect: redicte href

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -13,7 +13,11 @@ const SideNav = () => {
     const [windowHref, setWindowHref] = useState(false)
 
     if (windowHref) {
-        window.location = `${env.REACT_APP_EP_SITE}/auth/${stateAuth.token}`
+        if (process.env.NODE_ENV === 'production') {
+            window.location.href = `${process.env.REACT_APP_EP_SITE}/auth/${stateAuth.token}`
+        } else {
+            window.location.href = `${env.REACT_APP_EP_SITE}/auth/${stateAuth.token}`
+        }
     }
 
     console.log(env.REACT_APP_EP_SITE)
